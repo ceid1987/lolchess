273121352be01bda1b039ef0025bd74292380be3
MdeSOUSS <soussimathias@gmail.com>
2022-05-06 14:30:29 +0200

    chnagement pc souss

--- src/main/java/com/example/lolchess/champions/Champion.java
+++ src/main/java/com/example/lolchess/champions/Champion.java
@@ -3,11 +3,13 @@ package com.example.lolchess.champions;
 public abstract class Champion {
     protected String nom;
     protected int vie;
+    protected int equipe;
 
 
-    protected Champion(String nom, int vie) {
+    protected Champion(String nom, int vie, int equipe) {
         this.nom = nom;
         this.vie = vie;
+        this.equipe = equipe;
     }
 
     public String getNom() {
