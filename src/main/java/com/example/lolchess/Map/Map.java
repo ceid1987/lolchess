876edfe10250c4757aa747b92b9ec99c9cad037d
MdeSOUSS <soussimathias@gmail.com>
2022-05-06 14:41:21 +0200

    chnagement pc souss

---
+++ src/main/java/com/example/lolchess/Map/Map.java
@@ -0,0 +1,10 @@
+package com.example.lolchess.Map;
+
+public class Map {
+    public static void main(String[] args) {
+        int[][] matrix3 = { { 3, 2, 1, 7 },
+                { 9, 11, 5, 4 },
+                { 6, 0, 13, 17 },
+                { 7, 21, 14, 15 } };
+    }
+}
