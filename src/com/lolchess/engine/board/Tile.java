71ebd167c2431f2b0d73dda2c1ea38321eaca0fb
ceid1987 <c.eid.1312@gmail.com>
2022-05-25 05:19:46 +0200

    gui 4

--- src/com/lolchess/engine/board/Tile.java
+++ src/com/lolchess/engine/board/Tile.java
@@ -34,10 +34,6 @@ public abstract class Tile {
 
     public abstract Piece getPiece();
 
-    public int getTileCoordinate(){
-        return this.tileCoordinate;
-    }
-
     // subclasses
 
         // empty tile
